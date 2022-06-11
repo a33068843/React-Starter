a5599dffaf7efdd241ced5247c3e4d0fb395cc11
Benny Jou <a33068843@gmail.com>
2022-06-11 16:12:15 +0800

    feat: 新增初始的版面

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ResetStyle, GlobalStyle } from 'styles';
+import { Base } from 'pages';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ResetStyle />
+    <GlobalStyle />
+    <BrowserRouter>
+      <Base />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
